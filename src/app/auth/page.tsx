7e6ac95ef4853d1594ebdf3a5365921f25027cf8
Amaraa <amaraabokhbat@gmail.com>
2025-04-05 15:00:23 +0800

    amaraa

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,8 @@ import AuthForm from '../../components/Auth/AuthForm';
 export default function AuthPage() {
   return (
     <div className="min-h-screen bg-blue-50 flex items-center justify-center p-4">
-      <AuthForm />
+      {/* <AuthForm /> */}
+      hi
     </div>
   );
 }
