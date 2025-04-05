b414b433aae8fb43c38b94e10abee00d6a90a4a6
uuganaa <uugnaauugaa56@gmail.com>
2025-04-05 14:01:59 +0800

    del

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,6 @@ export default function Home() {
       <div className="flex-1">
         <div className="top-0 mb-6">
           <StorySection />
-          uuganaa
         </div>
         <div className="bg-white p-6 rounded-lg shadow-md mb-6">
           <h1 className="text-[36px] font-bold text-center mb-4">Find your <span className="text-teal-500 text-[36px] font-bold">Oyu</span>-Friend</h1>
